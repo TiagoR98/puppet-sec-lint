bb2c0c505c31207585f1bf8481a7d4bbef2c2c06
Tiago Ribeiro <TiagoJose.Ribeiro@ctw.bmwgroup.com>
2021-05-03 22:32:38 +0100

    Added recommendation pages

--- lib/sin_type.rb
+++ lib/sin_type.rb
@@ -2,36 +2,36 @@ var = module SinType
   HardCodedCred = {
     name: "Hard Coded Credentials",
     message: "Do not hard code secrets. This may help an attacker to attack the system.",
-    recommendation: "You can use hiera to avoid this issue."
+    solution: "https://tiagor98.github.io/puppet-sec-lint/hard-coded-credentials"
   }
   HttpWithoutTLS = {
     name: "HTTP without TLS",
     message: "Do not use HTTP without TLS. This may cause a man in the middle attack.",
-    recommendation: "Use TLS with HTTP"
+    solution: "https://tiagor98.github.io/puppet-sec-lint/http-without-tls"
   }
   AdminByDefault = {
     name: "Admin by default",
     message: "This violates the secure by design principle.",
-    recommendation: "Do not make default user as admin."
+    solution: "https://tiagor98.github.io/puppet-sec-lint/admin-by-default"
   }
   EmptyPassword = {
     name: "Empty password",
     message: "Do not keep password field empty. This may help an attacker to attack.",
-    recommendation: "You can use hiera to avoid this issue."
+    solution: "https://tiagor98.github.io/puppet-sec-lint/empty-password"
   }
   InvalidIPAddrBinding = {
     name: "Invalid IP Address Binding",
     message: "This config allows connections from every possible network.",
-    recommendation: "Restrict your available IPs."
+    solution: "https://tiagor98.github.io/puppet-sec-lint/invalid-ip-addr-binding"
   }
   SuspiciousComments = {
     name: "Suspicious Comments",
     message: "This comment can expose sensitive information to attackers.",
-    recommendation: "Do not expose sensitive information."
+    solution: "https://tiagor98.github.io/puppet-sec-lint/suspicious-comments"
   }
   WeakCryptoAlgorithm = {
     name: "Weak Crypto Algorithm",
     message: "Do not use this algorithm, as it may have security weaknesses.",
-    recommendation: "Use SHA-512 instead."
+    solution: "https://tiagor98.github.io/puppet-sec-lint/weak-crypto-algorithm"
   }
 end
